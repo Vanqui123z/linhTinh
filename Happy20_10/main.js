b9d07fccfb841baed8c2f6d76738a6e496ee2b18
VanQui <levanquyy1923@gmail.com>
2025-05-31 20:31:19 +0700

    update

--- Happy20_10/main.js
+++
@@ -1,9 +0,0 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-  setTimeout(() => {
-    document.getElementById('text').classList.add('visible');
-}, 2000);
